a21eb4c94ae3e49254c68b38902398cfd6eaaf11
kranthi-pula <kranthikumarpula@gmail.com>
2024-10-04 06:41:36 +0530

    First Commit

---
+++ src/Oct_03102024/Lab001.java
@@ -0,0 +1,7 @@
+package Oct_03102024;
+
+public class Lab001 {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/Oct_03102024/Lab003.java
@@ -0,0 +1,11 @@
+package Oct_03102024;
+
+public class Lab003 {
+
+    public static void main(String[] args){
+        int a = 3;
+        int b = 4;
+        int c = a+b;
+        System.out.println("a + b= " +c);
+    }
+}
